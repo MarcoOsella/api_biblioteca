3a71cc22f742d19f7ee966a7ebdad5b4249feff2
MarcoOsella <markosella34@gmail.com>
2023-07-25 14:56:53 -0300

    modified:   app.js origin 	modified:   middlewares/errorHandler.js origin 	modified:   package.json 	modified:   routes/libros.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
+const { auth } = require("express-oauth2-jwt-bearer");
+const errorHandler = require("./middlewares/errorHandler");
+// Configuracion Middleware con el Servidor de Autorización
+const jwtCheck = auth({
+  audience: 'http://127.0.0.1:3000/api/libros',
+  issuerBaseURL: 'https://dev-lir2hnalo5r6my50.us.auth0.com/',
+  tokenSigningAlg: 'RS256'
+});
 const app = express();
 app.use(express.json());
-
 // Importamos el Router de Libros
-const librosRouter = require('./routes/libros');
-
-// Importamos el Middleware Error Handler
-const errorHandler = require('./middlewares/errorHandler');
-
-app.use('/libros', librosRouter);
-
+const librosRouter = require("./routes/libros");
+//Configuramos el middleware de autenticacion
+app.use("/libros",  jwtCheck, librosRouter);
 app.use(errorHandler);
-
 app.listen(3000, () => {
-    console.log('Servidor iniciado en el puerto 3000');
+  console.log("Servidor iniciado en el puerto 3000");
 });
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,16 @@
+// middleware/errorHandler.js
 const errorHandler = (err, req, res, next) => {
-  console.error(err);
-  res
-    .status(err.status || 500)
-    .json({ error: err.message || "Error en el servidor" });
-};
-module.exports = errorHandler;
+  // Verificar si el error tiene un código de estado definido, de lo
+  //contrario, establecer el código de estado predeterminado
+  const statusCode = err.statusCode || 500;
+  // Construir objeto de respuesta de error
+  const errorResponse = {
+  error: {
+  message: err.message || "Error interno del servidor",
+  code: err.code || "internal_error",
+  },
+  };
+  // Enviar respuesta de error en formato JSON
+  res.status(statusCode).json(errorResponse);
+  };
+  module.exports = errorHandler;
